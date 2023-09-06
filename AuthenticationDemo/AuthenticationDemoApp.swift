b4f11faef743704e46129c3beb8c4aa39b5c2e58
KaitoSeita <113151647+KaitoSeita@users.noreply.github.com>
2023-09-06 22:40:35 +0900

    Initial Commit

---
+++ AuthenticationDemo/AuthenticationDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  AuthenticationDemoApp.swift
+//  AuthenticationDemo
+//
+//  Created by セイタカイト on 2023/09/06.
+//
+
+import SwiftUI
+
+@main
+struct AuthenticationDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
