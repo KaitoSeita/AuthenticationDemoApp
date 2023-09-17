f09736e8ece7c5cb20046eb06a183e6fd43161d0
kaitoseita <leadik2468@gmail.com>
2023-09-17 22:35:49 +0900

    AuthenticationTopViewのコードを修正（UIに変更なし）

--- AuthenticationDemo/UI/AuthenticationTopView.swift
+++ AuthenticationDemo/UI/AuthenticationTopView.swift
@@ -16,28 +16,48 @@ struct AuthenticationTopView: View {
                 .font(.system(.largeTitle, design: .rounded))
                 .bold()
             Spacer()
-            RoundedRectangle(cornerRadius: 20)
-                .frame(width: 330, height: 60)
-                .foregroundColor(.white)
-                .shadow(color: .white.opacity(0.8), radius: 10, x: -7, y: -7)
-                .shadow(color: .gray.opacity(0.3), radius: 10, x: 8, y: 8)
-                .overlay{
-                    Text(R.string.localizable.signInButton)
-                        .font(.system(size: 20, design: .rounded))
-                        .bold()
+            SignInButton()
+                .onTapGesture {
+                    // Routerへのイベント通知
                 }
-            RoundedRectangle(cornerRadius: 20)
-                .frame(width: 330, height: 60)
-                .foregroundColor(.black.opacity(0.85))
-                .shadow(color: .white.opacity(0.8), radius: 10, x: -7, y: -7)
-                .shadow(color: .gray.opacity(0.3), radius: 10, x: 8, y: 8)
-                .overlay{
-                    Text(R.string.localizable.signUpButton)
-                        .font(.system(size: 20, design: .rounded))
-                        .bold()
-                        .foregroundColor(.white)
+            SignUpButton()
+                .onTapGesture {
+                    // Routerへのイベント通知
                 }
         }
         .padding(EdgeInsets(top: 120, leading: 0, bottom: 150, trailing: 0))
+    }
+}
+
+private struct SignInButton: View {
+    
+    var body: some View {
+        RoundedRectangle(cornerRadius: 20)
+            .frame(width: 330, height: 60)
+            .foregroundColor(.white)
+            .shadow(color: .white.opacity(0.8), radius: 10, x: -7, y: -7)
+            .shadow(color: .gray.opacity(0.3), radius: 10, x: 8, y: 8)
+            .overlay{
+                Text(R.string.localizable.signInButton)
+                    .font(.system(size: 20, design: .rounded))
+                    .bold()
+            }
+    }
+}
+
+private struct SignUpButton: View {
+    
+    var body: some View {
+        RoundedRectangle(cornerRadius: 20)
+            .frame(width: 330, height: 60)
+            .foregroundColor(.black.opacity(0.85))
+            .shadow(color: .white.opacity(0.8), radius: 10, x: -7, y: -7)
+            .shadow(color: .gray.opacity(0.3), radius: 10, x: 8, y: 8)
+            .overlay{
+                Text(R.string.localizable.signUpButton)
+                    .font(.system(size: 20, design: .rounded))
+                    .bold()
+                    .foregroundColor(.white)
+            }
     }
 }
